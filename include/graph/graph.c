d46f5dcb282a2ac6dd6ac97bf21bc508937e1dab
jeanloubmn <73067971+jeanloupdb@users.noreply.github.com>
2023-04-20 15:53:35 +0200

    Update graph.c

--- include/graph/graph.c
+++ include/graph/graph.c
@@ -8,19 +8,8 @@
 #include <math.h>
 #include "graph.h"
 
-  
-/*struct PlanStock
-{ 
-	int nbRangees;
-	int nbCasiers;
-	int produit[][];
-};*/
-
-
 
 struct Graph G;
-
-
 
 void printNode(struct Node *node)
 {
